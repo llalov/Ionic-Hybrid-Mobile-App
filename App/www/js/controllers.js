ab203566ff7387bbdbd4c0b9f8349bb38f1b8b98
llalov <luchoazz@abv.bg>
2017-04-24 00:35:21 +0300

    initial commit

---
+++ App/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+  // App Controller
+})
+
+.controller('BooksCtrl', function($scope, $kinvey) {
+  // var store = $kinvey.DataStore.collection('books');
+  // store.useDeltaFetch = false;
+
+  // $scope.refresh = function() {
+  //   store.find().subscribe(function(books) {
+  //     $scope.books = books;
+  //     $scope.$digest();
+  //   });
+  // };
+
+  // $scope.$on('$ionicView.enter', function() {
+  //   $scope.books = [];
+  //   $scope.refresh();
+  // });
+});
