b62e21a61a2ab21a7387f223f1a1d43ba5bde39e
llalov <luchoazz@abv.bg>
2017-05-07 16:45:01 +0300

    update on the edit profile functionality

--- App/www/js/controllers/MyProfileController.js
+++ App/www/js/controllers/MyProfileController.js
@@ -36,41 +36,52 @@ angular.module('app.me', [])
                     .catch(function(error) {
                         return error.data;
                     })
+
+                $scope.changeProfilePic = function() {
+                
+                    var metadata = {
+                        userId: $scope.userInfo._id,
+                        _acl: {
+                            w: [$scope.userInfo._id]
+                        },
+                        _public: "true"
+                    };
+
+                    var file = document.getElementById('file').files[0];
+                    
+                    //   Uploading the image in the database
+                    var promise = $kinvey.Files.upload(file, metadata)
+                        .then(function(file) {
+                            //  Finding old profile picture id by the used Id
+                            var query = new $kinvey.Query();
+                            query.equalTo('userId', $scope.userInfo._id);
+                            var promise = $kinvey.Files.find(query)
+                                .then(function(file) {
+                                    console.log("Old profile pic: "+ file);
+                                    console.log(file['0']._id);
+                                    //  Removing the old image from the database
+                                    var promise = $kinvey.Files.removeById(file['0']._id)
+                                        .then(function() {
+                                            console.log("Old picture is replaced by the new one")
+                                        })
+                                        .catch(function(error) {
+                                            console.log("Error in upload: " + error)
+                                        });
+                                })
+                                .catch(function(error) {
+                                    console.log("Invalid QUERY");
+                                });
+
+                            console.log("Successfull upload")
+                        })
+                        .catch(function(error) {
+                            console.log("Upload error: "+ error);
+                    });
+                }
                 
             }
             
-            $scope.uploadFile = function() {
-              var metadata = {
-                userId: $scope.userInfo._id 
-              };
 
-              var file = document.getElementById('file').files[0];
-              var promise = $kinvey.Files.upload(file, metadata)
-                .then(function(file) {
-                    var query = new $kinvey.Query();
-                    query.equalTo('userId', $scope.userInfo._id);
-                    var promise = $kinvey.Files.find(query)
-                        .then(function(file) {
-                            console.log("ФАЙЛЧЕТО ДЕЙБА: "+ file);
-                            console.log(file['0']._id);
-                            var promise = $kinvey.Files.removeById(file['0']._id)
-                                .then(function() {
-                                    console.log("Старата картинка е махната")
-                                 })
-                                .catch(function(error) {
-                                    console.log("Старата картинка НЕ е махната")
-                                });
-                        })
-                        .catch(function(error) {
-                            console.log("НЕВАЛИДНО QUERY");
-                        });
-
-                    console.log("Y")
-                })
-                .catch(function(error) {
-                    console.log("N");
-              });
-            }
             
             $scope.$on('$ionicView.enter', function() {
                 $scope.userInfo = [];
